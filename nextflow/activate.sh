097141c38e862d1f979e500325014dc0baa4ff71
loAlon <loalon@outlook.es>
2018-07-20 17:42:27 +0200

    Nextflow pipeline initial

---
+++ nextflow/activate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+module load conda
+source activate nextflow
+module load bioinfo-tools
+module load FastQC
+module load sortmerna
+module load Trimmomatic
+module load funmerge
+module load multiqc
+module load salmon
+
