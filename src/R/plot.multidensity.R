3488f24bff4b83d041b35ceb54297bedecb847a7
Nicolas Delhomme <nicolas.delhomme@umu.se>
2019-06-29 23:07:15 +0200

    modified the multidensity plot

--- src/R/plot.multidensity.R
+++ src/R/plot.multidensity.R
@@ -1,34 +1,90 @@
-"plot.multidensity" <- function(x,xlab="x",col=brewer.pal(8,"Dark2"),
-                                legend.x="top",xlim=NULL,ylim=NULL,
-                                lty=1,legend.cex=1,...){
+#' ---
+#' title: "Multi density plot"
+#' author: "Nicolas Delhomme"
+#' date: "`r Sys.Date()`"
+#' output:
+#'  html_document:
+#'    toc: true
+#'    number_sections: true
+#' ---
 
-  ## pckg
-  library(RColorBrewer)
+stopifnot(suppressPackageStartupMessages(require(RColorBrewer)))
+
+setGeneric(name="plot.multidensity",
+           def=function(x,
+                        xlab="x",col=brewer.pal(8,"Dark2"),
+                        legend.x="top",xlim=NULL,ylim=NULL,
+                        lty=1,legend.cex=1,legend=NULL,
+                        legend.col=NULL,legend.lwd=1,...){
+  standardGeneric("plot.multidensity")
+})
+
+setMethod(f="plot.multidensity",
+          signature=c("list"),
+          definition=function(x,
+                              xlab="x",col=brewer.pal(8,"Dark2"),
+                              legend.x="top",xlim=NULL,ylim=NULL,
+                              lty=1,legend.cex=1,legend=NULL,
+                              legend.col=NULL,legend.lwd=1,...){
+            
+            ## densities
+            dens <- lapply(x,density)
+            .plot.multidensity(dens,xlab,col,legend.x,xlim,ylim,
+                               lty,legend.cex,legend,legend.col,legend.lwd)
+          })
+
+setMethod(f="plot.multidensity",
+          signature=c("matrix"),
+          definition=function(x,
+                              xlab="x",col=brewer.pal(8,"Dark2"),
+                              legend.x="top",xlim=NULL,ylim=NULL,
+                              lty=1,legend.cex=1,legend=NULL,
+                              legend.col=NULL,legend.lwd=1,...){
+            
+            ## densities
+            dens <- apply(x,2,density)
+            .plot.multidensity(dens,xlab,col,legend.x,xlim,ylim,
+                               lty,legend.cex,legend,legend.col,legend.lwd)
+          })
+
+".plot.multidensity" <- function(d, xlab="x",col=brewer.pal(8,"Dark2"),
+                                 legend.x="top",xlim=NULL,ylim=NULL,
+                                 lty=1,legend.cex=1,legend=NULL,
+                                 legend.col=NULL,legend.lwd=1,...){
   
-  ## check
-  stopifnot(is.list(x))
-  
-  ## densities
-  dens <- lapply(x,density)
   if(is.null(xlim)){
-    xlim <- range(sapply(dens,"[[","x"))
+    xlim <- range(sapply(d,"[[","x"))
   }
+  
   if(is.null(ylim)){
-    ylim <- range(sapply(dens,"[[","y"))
+    ylim <- range(sapply(d,"[[","y"))
   }
-
+  
   ## lty
   if(length(lty)==1){
-    lty <- rep(lty,length(dens))
+    lty <- rep(lty,length(d))
   }
   
   ## plot
   plot(0,0,xlim=xlim,ylim=ylim,ylab="density",type="n",xlab=xlab,...)
   
   ## lines
-  sapply(1:length(dens),function(i,dens,col,...){lines(dens[[i]],col=col[i],lty=lty[i],...)},dens,col,...)
+  sapply(1:length(d),function(i,d,col,...){lines(d[[i]],col=col[i],lty=lty[i],...)},d,col,...)
   
   ## legend
-  legend(legend.x,col=col[1:length(x)],bty="n",legend=names(x),lty=lty,cex=legend.cex)
+  if(!is.null(legend) | !is.null(names(d))){
+    if(is.null(legend)){
+      leg <- names(d)
+    } else {
+      leg <- legend
+    }
+    if(!is.null(legend.col)){
+      lcol <- legend.col
+    } else {
+      lcol <- col[1:length(d)]
+    }
+    legend(legend.x,col=lcol,bty="n",
+           legend=leg,lty=lty,cex=legend.cex,
+           lwd=legend.lwd)
+  }
 }
-
